cb729b9e3fd196deb245898ad21dd3d4ae207c90
Marija Milevska <marija.milevska05@gmail.com>
2024-08-01 13:36:53 +0200

    fix: update instructions to include WASD controls for movement

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       <h1>Path Finder</h1>
       <div>
-        <p>Use the buttons or keyboard arrows to follow the path!</p>
+        <p>Use the buttons or "W", "A", "S", "D" keys to follow the path!</p>
       </div>
       <PathFinder />
     </div>
